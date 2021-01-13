394fd22945ddecfd901d657abb2dd2bc8918bf5d
Colby Robinson <colbyrobinson@Colbys-iMac.local>
2021-01-13 17:00:35 +0000

    calculator

--- Drumkit/app.js
+++ Drumkit/app.js
@@ -1,16 +1,26 @@
-const keyValueId =document.getElementById("keyValue")
-const KeyCodeId =document.getElementById("clap")
-const CharacterCodeId =document.getElementById("charCode")
+document.addEventListener('keydown', function(e){
 
 
-window.addEventListener('keypress', (event) => {
-    console.log(event)
+const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`)
 
-    keyValueId.innerHTML = event.key;
-    KeyCodeId.innerHTML = event.code;
-    CharacterCodeId.innerHTML = event.keyCode;
+const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`)
+
+if (!audio){
+    return
+}
+
+audio.currentTime = 0
+audio.play()
+key.classList.add('playing')
 })
 
-// document.querySelector('.wrap').classList.toggle('hide');
-// document.querySelector('.keycode-display').classList.toggle('hide');
-// table.classList.toggle('hide');
+const removeTransition = function(e){
+    if (e.propertyName !== 'transform')
+    return
+        this.classList.remove('playing')
+      }
+      const keys = document.querySelectorAll('.key')
+      console.log(keys)
+      keys.forEach(function(key){
+        key.addEventListener('transitionend', removeTransition)
+      })
